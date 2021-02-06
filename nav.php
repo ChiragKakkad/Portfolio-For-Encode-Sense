8bda8351697bea1ecf685dd66e648ded73f41ed0
Chirag Kakkad <chiragkakkadofficial@yahoo.com>
2021-02-06 15:27:13 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,263 @@
+<html>
+
+<head>
+    <title>Encode Sense</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <i class="feather icon-feather-search"></i>
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="border-box">
+        <h1>Encode Sense</h1>
+    </div>
+
+    <ul class="portfolio-filter grid-filter nav nav-tabs justify-content-center border-0 text-uppercase font-weight-500 alt-font padding-6-rem-bottom md-padding-4-half-rem-bottom sm-padding-2-rem-bottom">
+        <li class="nav active"><a data-filter="*" href="#">All</a></li>
+        <li class="nav"><a data-filter=".branding" href="#">Branding</a></li>
+        <li class="nav"><a data-filter=".graphics" href="#">Graphics</a></li>
+        <li class="nav"><a data-filter=".logos" href="#">Logos</a></li>
+        <li class="nav"><a data-filter=".photography" href="#">Photography</a></li>
+    </ul>
+
+    <section class="ftco-section-2">
+        <div class="photograhy">
+            <div class="row no-gutters">
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/liveclefs.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/liveclefs.png); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 01</h3>
+                            <span class="tag">LIVECLEFS WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/treatmentscout.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/treatmentscout.png); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 02</h3>
+                            <span class="tag">TREATMENT SCOUT WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/topdocs-net.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/topdocs-net.png); background-size:100%">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 03</h3>
+                            <span class="tag">TOPDOCS-NET WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/electriccyclery.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/electriccyclery.png); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 04</h3>
+                            <span class="tag">ELECTRIC CYCLERY WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/cyclonetaylor.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/cyclonetaylor.png); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 05</h3>
+                            <span class="tag">CYCLONE TAYLOR WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="bouncedeterminant.png" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/bouncedeterminant.png); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 06</h3>
+                            <span class="tag">BOUNCE DETERMINANT WEBSITE</span>
+                        </div>
+                    </a>
+                </div>
+<!--
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_7.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_7.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 07</h3>
+                            <span class="tag">Fashion, Model</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_8.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_8.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 08</h3>
+                            <span class="tag">Nature</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_9.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_9.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 09</h3>
+                            <span class="tag">Technology</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_10.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_10.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 10</h3>
+                            <span class="tag">Model</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_11.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_11.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 11</h3>
+                            <span class="tag">Fashion</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                    <a href="images/image_12.jpg" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(images/image_12.jpg); background-size:100%;">
+                        <div class="overlay"></div>
+                        <div class="text text-center">
+                            <h3>Work 12</h3>
+                            <span class="tag">Photography</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+-->
+        </div>
+    </section>
+    
+    
+<!----------------------FOOTERS---------------------->
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="footer-about">
+                        <h2>About Us</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet metus sit amet diam varius commodo. Aliquam at nisl interdum
+                        </p>
+                        <br>
+                        <p><i class="fa fa-map-marker-alt"></i>123 Street, New York, USA</p>
+                        <p><i class="fa fa-phone-alt"></i>+91 98245 18281</p>
+                        <p><i class="fa fa-envelope"></i>Encodesense@gmail.com</p>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="footer-link">
+                                <h2>Useful Link</h2>
+                                <a href="#">About Us</a>
+                                <a href="#">Our Story</a>
+                                <a href="#">Our Services</a>
+                                <a href="#">Our Portfolio</a>
+                                <a href="#">Our Projects</a>
+                                <a href="#">Contact Us</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="footer-link">
+                                <h2>Useful Link</h2>
+                                <a href="#">Our Clients</a>
+                                <a href="#">Clients Review</a>
+                                <a href="#">Ongoing Customer</a>
+                                <a href="#">Customer Support</a>
+                                <a href="#">FAQs</a>
+                                <a href="#">Site Map</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container copyright">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; <a href="#">Encode Sense</a>, All Right Reserved.</p>
+                </div>
+                <div class="col-md-6">
+                    <p>Services By<a href="#"> Encode Sense</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min"></script>
+    <script src="js/bootstrap.min"></script>
+
+
+
+    <!--
+    <div class="container-ulli">
+        <ul class="elevul">
+            <li class="elevli">
+                <div class="elevate"></div>
+            </li>
+            <li class="elevli">
+                <div class="elevate"></div>
+            </li>
+            <li class="elevli">
+                <div class="elevate"></div>
+            </li>
+        </ul>
+    </div>
+-->
+
+</body>
+
+</html>
